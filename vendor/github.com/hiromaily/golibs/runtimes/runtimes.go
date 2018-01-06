c3d8f2c249e28750642e3df3b1b98fe545c23e23
hiromaily <hiromaily2@gmail.com>
2018-01-06 11:26:57 +0100

    update vendor

--- vendor/github.com/hiromaily/golibs/runtimes/runtimes.go
+++ vendor/github.com/hiromaily/golibs/runtimes/runtimes.go
@@ -2,9 +2,72 @@ package runtimes
 
 import (
 	"bytes"
+	"fmt"
+	"io"
+	"regexp"
 	"runtime"
 	"strings"
 )
+
+var (
+	re = regexp.MustCompile(`^(\S.+)\.(\S.+)$`)
+)
+
+type CallerInfo struct {
+	PackageName  string
+	FunctionName string
+	FileName     string
+	FileLine     int
+}
+
+func formatPath(path, separator string) string {
+	ret := strings.Split(path, separator)
+	if len(ret) > 1 {
+		return fmt.Sprintf(".%s", ret[len(ret)-1])
+	}
+	return path
+}
+
+func dumpStackTrace(separator string) (callerInfo []*CallerInfo) {
+	for i := 1; ; i++ {
+		pc, _, _, ok := runtime.Caller(i) // https://golang.org/pkg/runtime/#Caller
+		if !ok {
+			break
+		}
+
+		fn := runtime.FuncForPC(pc)
+		fileName, fileLine := fn.FileLine(pc)
+
+		// format path
+		if separator != "" {
+			fileName = formatPath(fileName, separator)
+		}
+
+		additionalInfo := re.FindStringSubmatch(fn.Name())
+		callerInfo = append(callerInfo, &CallerInfo{
+			PackageName:  additionalInfo[1],
+			FunctionName: additionalInfo[2],
+			FileName:     fileName,
+			FileLine:     fileLine,
+		})
+	}
+	return callerInfo[1:]
+}
+
+func GetStackTrace(separator string) []*CallerInfo {
+	info := dumpStackTrace(separator)
+	return info
+}
+
+func TraceAllHistory(w io.Writer, separator string) {
+	info := dumpStackTrace(separator)
+	for i := len(info) - 1; i > -1; i-- {
+		v := info[i]
+		//fmt.Printf("%02d: %s%s@%s:%d\n", i, v.PackageName, v.FunctionName, v.FileName, v.FileLine)
+		//fmt.Printf("%02d: [Function]%s [File]%s:%d\n", i, v.FunctionName, v.FileName, v.FileLine)
+		fmt.Fprintf(w, "%02d: [Function]%s [File]%s:%d\n", i, v.FunctionName, v.FileName, v.FileLine)
+	}
+}
 
 // CurrentFunc is to get current func name
 func CurrentFunc(skip int) string {
