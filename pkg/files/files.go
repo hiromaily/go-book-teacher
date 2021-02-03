7f78431bbadd0d45cfc88335b8f65f9c8afc6d48
HirokiYasui <hiromaily2@gmail.com>
2021-02-03 20:44:51 +0900

    update

--- pkg/files/files.go
+++ pkg/files/files.go
@@ -3,12 +3,12 @@ package files
 import (
 	"fmt"
 	"os"
-	"os/exec"
-	"strings"
 
 	"github.com/hiromaily/go-book-teacher/pkg/config"
 	"github.com/hiromaily/go-book-teacher/pkg/teachers"
 )
+
+const installPath = "/usr/local/etc/book-teacher/"
 
 // GetConfigPath returns toml file path
 func GetConfigPath(tomlPath string) string {
@@ -16,13 +16,17 @@ func GetConfigPath(tomlPath string) string {
 		return tomlPath
 	}
 	// book-teacher.toml
-	targetDir, err := getBinDir()
-	if err == nil {
-		expectedFileName := fmt.Sprintf("%s%s.toml", targetDir, os.Args[0])
-		if isExist(expectedFileName) {
-			return expectedFileName
-		}
+	if installed := getInstallPath("toml"); isExist(installed) {
+		return installed
 	}
+	//targetDir, err := getBinDir()
+	//if err == nil {
+	//	expectedFileName := fmt.Sprintf("%s%s.toml", targetDir, os.Args[0])
+	//	if isExist(expectedFileName) {
+	//		return expectedFileName
+	//	}
+	//}
+
 	envFile := config.GetEnvConfPath()
 	if envFile != "" && isExist(envFile) {
 		return envFile
@@ -36,13 +40,16 @@ func GetJSONPath(jsonPath string) string {
 		return jsonPath
 	}
 	// book-teacher.json
-	targetDir, err := getBinDir()
-	if err == nil {
-		expectedFileName := fmt.Sprintf("%s%s.json", targetDir, os.Args[0])
-		if isExist(expectedFileName) {
-			return expectedFileName
-		}
+	if installed := getInstallPath("json"); isExist(installed) {
+		return installed
 	}
+	//targetDir, err := getBinDir()
+	//if err == nil {
+	//	expectedFileName := fmt.Sprintf("%s%s.json", targetDir, os.Args[0])
+	//	if isExist(expectedFileName) {
+	//		return expectedFileName
+	//	}
+	//}
 
 	envFile := teachers.GetEnvJSONPath()
 	if envFile != "" && isExist(envFile) {
@@ -62,11 +69,15 @@ func isExist(file string) bool {
 	return true
 }
 
-func getBinDir() (string, error) {
-	out, err := exec.Command("which", []string{os.Args[0]}...).Output()
-	if err != nil {
-		return "", err
-	}
-	// FIXME: windows newline is \r\n
-	return strings.TrimRight(string(out), fmt.Sprintf("%s\n", os.Args[0])), nil
+func getInstallPath(ext string) string {
+	return fmt.Sprintf("%s%s.%s", installPath, os.Args[0], ext)
 }
+
+//func getBinDir() (string, error) {
+//	out, err := exec.Command("which", []string{os.Args[0]}...).Output()
+//	if err != nil {
+//		return "", err
+//	}
+//	// FIXME: windows newline is \r\n
+//	return strings.TrimRight(string(out), fmt.Sprintf("%s\n", os.Args[0])), nil
+//}
